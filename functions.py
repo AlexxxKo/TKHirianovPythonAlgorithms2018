72f7f1885adadbf8dd15cc06b153095168258209
Alex Ko <help.mail015@gmail.com>
2026-05-13 19:27:40 +0300

    added task1_4: Turtle. Circle without circle()

--- functions.py
+++ functions.py
@@ -1,10 +1,19 @@
 import turtle as t
 
-
 def getIntNumber(question: str) -> int:
     while True:
         try:
             n = int(input(f'{question}: '))
+            break
+        except ValueError:
+            print('Нужно ввести целое число\n')
+
+    return n
+
+def getFloatNumber(question: str) -> int:
+    while True:
+        try:
+            n = float(input(f'{question}: '))
             break
         except ValueError:
             print('Нужно ввести число\n')
---
+++ task1_4.py
@@ -0,0 +1,21 @@
+# Нарисуйте окружность, не используя circle()
+
+import turtle as t
+from functions import getFloatNumber, runTurtle
+from math import pi
+
+
+def run():
+    r = getFloatNumber('Введите радиус окружности')
+    l = 2 * pi * r
+    n = 100
+    angle = 360 / n
+    side = l / n
+
+    t.shape('turtle')
+    t.left(90)
+    for i in range(n):
+        t.forward(side)
+        t.left(angle)
+
+runTurtle(run)
